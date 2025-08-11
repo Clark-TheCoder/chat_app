c8fdf543d21f502486a8391f64b3c4fd3bd70c17
Emma Clark <emmabossuytclark@gmail.com>
2025-08-11 13:56:40 -0300

    Completed functionality to add local audio and video to the room prior to connecting to the room. Created peerConnection and attached local streams to it.

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,36 @@
+import { createVideo } from "/js/createVideo.js";
+
 const socket = io("/");
 
-socket.emit("join-room", roomID);
+let localStream;
+
+async function start() {
+  try {
+    //get and append the streams of the local user to the room
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+    localStream = stream;
+    const myVideo = createVideo();
+    myVideo.muted = true; //mute to prevent feedback
+    myVideo.srcObject = stream;
+
+    //join room
+    socket.emit("join-room", roomID);
+  } catch (error) {
+    console.error("Error accessing media devices:", error);
+  }
+}
+
+start();
 
 socket.on("user-connected", (userId) => {
-  console.log(userId);
+  //create peerConnection
+  const peerConnection = new RTCPeerConnection();
+
+  // Add your local tracks
+  localStream.getTracks().forEach((track) => {
+    peerConnection.addTrack(track, localStream);
+  });
 });
